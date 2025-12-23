179c725fec6860754036f8d2719e144bbe092ee0
OCE1960 <chukwuemeka.christian.okeke@gmail.com>
2025-12-23 22:22:45 +0000

    update to settings

--- apms/data/models/role.py
+++ apms/data/models/role.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django.utils import timezone
 
 class Role(models.Model):
     name = models.CharField(max_length=50)
     display_name = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-        return f"{self.key} - {self.status}"
+        return f"{self.name} - {self.display_name}"
--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "apms.apps.ApmsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -115,3 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+MIGRATION_MODULES = {"apms": "apms.data.migrations"}
+
+AUTH_USER_MODEL = "apms.User"
