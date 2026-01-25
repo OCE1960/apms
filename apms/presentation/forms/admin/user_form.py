5da1b79620ce3b75bf4848ed110ca892b163a210
OCE1960 <chukwuemeka.christian.okeke@gmail.com>
2026-01-25 23:44:35 +0000

    Added a presentation layer for the application

---
+++ apms/presentation/forms/admin/user_form.py
@@ -0,0 +1,93 @@
+from django import forms
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
+
+class UserCreationForm(forms.Form):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Password confirmation", widget=forms.PasswordInput
+    )
+    email = forms.EmailField(label="email address", max_length=255)
+    date_of_birth = forms.DateField()
+    first_name = forms.CharField(max_length=60)
+    middle_name = forms.CharField(max_length=60)
+    last_name = forms.CharField(max_length=60)
+    roles = forms.MultipleChoiceField(queryset=None)
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+                                               
+class UserChangeForm(forms.Form):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    disabled password hash display field.
+    """
+
+    password = ReadOnlyPasswordHashField()
+    email = forms.EmailField(label="email address", max_length=255)
+    date_of_birth = forms.DateField()
+    first_name = forms.CharField(max_length=60)
+    middle_name = forms.CharField(max_length=60)
+    last_name = forms.CharField(max_length=60)
+    roles = forms.MultipleChoiceField(queryset=None)
+
+
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    # inlines = [RoleUserInline]
+    list_display = ["first_name", "last_name", "email", "date_of_birth", "is_admin"]
+    list_display_links = ["first_name", "last_name", "email"]
+    list_filter = ["is_admin", "roles"]
+    sortable_by = ["first_name", "last_name", "email", "date_of_birth"]
+    fieldsets = [
+        (None, {"fields": ["email", "password"]}),
+        ("Personal info", {"fields": ["first_name", "last_name", "date_of_birth"]}),
+        ("Permissions", {"fields": ["is_admin"]}),
+    ]
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": ["first_name", "last_name", "email", "date_of_birth", "password1", "password2"],
+            },
+        ),
+    ]
+    search_fields = ["email", "first_name", "last_name", "roles__label"]
+    ordering = ["email"]
+    filter_horizontal = []
+    view_on_site = True
+    # change_form_template = 'cms/index.html'
+    
+    class Media:
+        # css = {
+        #     "all": ["apms/my_styles.css"],
+        # }
+        # js = ["my_code.js"]
+        pass
+
